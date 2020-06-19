9fd7d0c77f8a7637487f7ee324da1978255ab721
Shaukat Ali <shaukatwattoo921@gmail.com>
2020-06-19 23:42:26 +0500

    created react state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Message from './message.js';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [isCredit, setCredit] = useState(true);
+
   return (
-    <div>
-      Hello World
+    <div className= {`box ${isCredit ? 'daylight' : ''}`}>
+      <h1 className= {'country'}> پاکستان زندہ آباد </h1>
+      
+      <h1> {isCredit ? 'Congratulation! You are eligible to Apply' : 'Sorry, you are Not Eligible due to Low Credit'}</h1>
+      
+      <button onClick={() => setCredit(!isCredit)} className= {'updateCredit'}>
+        Update Credit
+      </button>
+      <br />
+      <Message counter={count} />
+      
+      <button onClick={() => setCount(count + 1  ) } className= {'updateCounter'}>
+        Increase Value 
+      </button>
+      <br />
+      <button onClick={() => setCount(count - 1)} className= {'decrease'}>
+        Decrease Value
+      </button>
+      
     </div>
   );
 }
